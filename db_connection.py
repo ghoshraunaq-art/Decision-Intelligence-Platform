2d0e1431813935a54b46cf7d0debfec14b17a073
RAUNAQ GHOSH <ghoshraunaq@gmail.com>
2026-07-14 23:35:04 +0530

    Add files via upload

---
+++ db_connection.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import psycopg2
+
+
+def get_connection():
+    connection = psycopg2.connect(
+        host=st.secrets["DB_HOST"],
+        database=st.secrets["DB_NAME"],
+        user=st.secrets["DB_USER"],
+        password=st.secrets["DB_PASSWORD"],
+        port=st.secrets["DB_PORT"],
+        sslmode="require"
+    )
+    return connection
