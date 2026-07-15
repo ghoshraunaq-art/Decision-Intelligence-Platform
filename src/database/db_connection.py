61f396115ed128e14a55f8bc57b80711f422e682
Raunaq Ghosh <ghoshraunaq@gmail.com>
2026-07-15 20:08:02 +0530

    Fix Supabase connection using connection pooler

--- src/database/db_connection.py
+++ src/database/db_connection.py
@@ -8,6 +8,7 @@ def get_connection():
         database=st.secrets["DB_NAME"],
         user=st.secrets["DB_USER"],
         password=st.secrets["DB_PASSWORD"],
-        port=st.secrets["DB_PORT"]
+        port=st.secrets["DB_PORT"],
+        sslmode="require"
     )
     return connection
